75b5e4667aa5511137e49982b70338333ae455b6
GaspLEV <gaspar.leveque@gmail.com>
2024-03-20 11:23:19 +0100

    upload images in src

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function ListeDeroulante() {
+    const [selectedOption, setSelectedOption] = useState("https://exemple.com/chemin/vers/coconut.jpg"); 
+
+    const handleChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+
+    return (
+        <div>
+            <select value={selectedOption} onChange={handleChange}>
+                <option value="https://exemple.com/chemin/vers/banana.jpg">Banane</option>
+                <option value="https://exemple.com/chemin/vers/lime.jpg">Citron vert</option>
+                <option value="https://exemple.com/chemin/vers/coconut.jpg">Noix de coco</option>
+                <option value="https://exemple.com/chemin/vers/mango.jpg">Mangue</option>
+            </select>
+            <p>Option sélectionnée: {selectedOption}</p>
+            <img src={selectedOption} alt="Image sélectionnée"></img>
+        </div>
+    );
 }
 
-export default App;
+
