a73d8b56fbc6bb4e44ef2e5c509c031518b9f4ae
dolphounet <maxencepj.04@gmail.com>
2024-03-22 11:05:07 +0100

    added signup page

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,15 +1,17 @@
 import { useState } from "react";
 import "./LoginScreen.css";
 
-export default function LoginScreen() {
+function Login({ switchMethod, display }) {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
   function handleSubmit(event) {
     event.preventDefault();
-    alert(password);
+    if (password) {
+      alert(password);
+    }
   }
   return (
-    <div className="Login-Screen">
+    <div className="Login-Screen" style={{ display: display }}>
       <h1>Login</h1>
       <form className="Login-Form" onSubmit={handleSubmit}>
         <div className="Form-Item">
@@ -32,6 +34,82 @@ export default function LoginScreen() {
           <input type="submit" value="Log in" />
         </div>
       </form>
+      <p>
+        <label>No account ?</label>
+        <button onClick={switchMethod}>create one.</button>
+      </p>
+    </div>
+  );
+}
+
+function Signup({ switchMethod, display }) {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+  const [passwordConfirm, setPasswordConfirm] = useState();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (password && password === passwordConfirm) {
+      alert(password);
+    }
+  }
+  return (
+    <div className="Login-Screen" style={{ display: display }}>
+      <h1>Signup</h1>
+      <form className="Form" onSubmit={handleSubmit}>
+        <div className="Form-Item">
+          <label>Username</label>
+          <input
+            type="text"
+            placeholder="Enter your username"
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <div className="Form-Item">
+          <label>Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="Form-Item">
+          <label>Confirm your password</label>
+          <input
+            type="password"
+            placeholder="Confirm your password"
+            onChange={(e) => setPasswordConfirm(e.target.value)}
+          />
+        </div>
+        <div className="Form-Item">
+          <input type="submit" value="Sign up" />
+        </div>
+      </form>
+      <p>
+        <label>Already has an account ? </label>
+        <button onClick={switchMethod}>connect.</button>
+      </p>
+    </div>
+  );
+}
+
+export default function LoginScreen() {
+  const [display, setDisplay] = useState(["", "none"]);
+  function switchMethod() {
+    const nextDisplay = display.slice();
+    if (nextDisplay[0] === "none") {
+      nextDisplay[0] = "";
+      nextDisplay[1] = "none";
+    } else {
+      nextDisplay[0] = "none";
+      nextDisplay[1] = "";
+    }
+    setDisplay(nextDisplay);
+  }
+  return (
+    <div>
+      <Login display={display[0]} switchMethod={switchMethod} />
+      <Signup display={display[1]} switchMethod={switchMethod} />
     </div>
   );
 }
