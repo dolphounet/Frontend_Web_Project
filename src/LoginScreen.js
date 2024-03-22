2e35f827d1deaeb7b470dc7749e1dbd33a6afcd5
dolphounet <maxencepj.04@gmail.com>
2024-03-22 02:40:22 +0100

    added primitive logic to login screen

--- src/LoginScreen.js
+++ src/LoginScreen.js
@@ -1,28 +1,35 @@
+import { useState } from "react";
 import "./LoginScreen.css";
 
 export default function LoginScreen() {
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(password);
+  }
   return (
     <div className="Login-Screen">
       <h1>Login</h1>
-      <form className="Login-Form" onSubmit={console.log("certes")}>
+      <form className="Login-Form" onSubmit={handleSubmit}>
         <div className="Form-Item">
           <label>Username</label>
           <input
             type="text"
-            name="username"
             placeholder="Enter your username"
+            onChange={(e) => setUserName(e.target.value)}
           />
         </div>
         <div className="Form-Item">
           <label>Password</label>
           <input
             type="password"
-            name="password"
             placeholder="Enter your password"
+            onChange={(e) => setPassword(e.target.value)}
           />
         </div>
         <div className="Form-Item">
-          <input type="submit" name="" value="Log in" />
+          <input type="submit" value="Log in" />
         </div>
       </form>
     </div>
