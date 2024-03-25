8e2903c9090518b72b4cce1d5b23268dd64d9960
dolphounet <maxencepj.04@gmail.com>
2024-03-25 09:48:57 +0100

    added a grid component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 //import App from "./App.test";
 //import reportWebVitals from "./reportWebVitals";
 import Menu from "./components/Menu";
+import Grid from "./components/Grid";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Menu />
-  </React.StrictMode>,
+  <StrictMode>
+    <Grid knightPosition={[7, 4]} />
+  </StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
